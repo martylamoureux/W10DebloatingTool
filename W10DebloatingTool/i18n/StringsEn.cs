91e30e7c24248b196f1febd17579853af4d09e91
Marty Lamoureux <marty@lamoureux-mail.fr>
2015-08-11 19:35:50 +0200

    Switched to .NET 4.0 and removed properties expression body syntax

--- W10DebloatingTool/i18n/StringsEn.cs
+++ W10DebloatingTool/i18n/StringsEn.cs
@@ -3,11 +3,24 @@ namespace W10DebloatingTool.i18n
 {
     public class StringsEn : IStrings
     {
-        public string LetsGo => "Let's Go !";
-        public string SaveLogs => "Save Logs";
+        public string LetsGo
+        {
+            get { return "Let's Go !"; }
+        }
 
-        public string Privacy => "Privacy";
+        public string SaveLogs
+        {
+            get { return "Save Logs"; }
+        }
 
-        public string Applications => "Applications";
+        public string Privacy
+        {
+            get { return "Privacy"; }
+        }
+
+        public string Applications
+        {
+            get { return "Applications"; }
+        }
     }
 }
--- W10DebloatingTool/i18n/StringsFr.cs
+++ W10DebloatingTool/i18n/StringsFr.cs
@@ -3,11 +3,24 @@ namespace W10DebloatingTool.i18n
 {
     public class StringsFr : IStrings
     {
-        public string LetsGo => "Allons-y !";
-        public string SaveLogs => "Enregistrer les Logs";
+        public string LetsGo
+        {
+            get { return "Allons-y !"; }
+        }
 
-        public string Privacy => "Confidentialité";
+        public string SaveLogs
+        {
+            get { return "Enregistrer les Logs"; }
+        }
 
-        public string Applications => "Applications";
+        public string Privacy
+        {
+            get { return "Confidentialité"; }
+        }
+
+        public string Applications
+        {
+            get { return "Applications"; }
+        }
     }
 }
